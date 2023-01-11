7fb1a2f67242e84339d563f3ec18028dc57de75c
Lucas-ASouza <lucasalves-x@hotmail.com>
2023-01-11 08:34:15 -0300

    feat: now have a task that create a basic population for the database using seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  Coin.create!(
+       description: "bitcoin",
+       acronym: "BTC",
+       url_image: "https://w7.pngwing.com/pngs/232/592/png-transparent-btc-crypto-cryptocurrency-cryptocurrencies-cash-money-bank-payment-icon.png"    
+)
+   Coin.create!(
+       description: "Etherum",
+       acronym: "ETH",
+       url_image: "https://img1.gratispng.com/20180516/ouw/kisspng-ethereum-cryptocurrency-blockchain-logo-eos-io-crypto-5afc9ab9b20d86.8643914515265041217293.jpg"    
+)
+   Coin.create!(
+       description: "Dash",
+       acronym: "DASH",
+       url_image: "https://cdn4.iconfinder.com/data/icons/crypto-currency-and-coin-2/256/dash_dashcoin-512.png"    
+)
