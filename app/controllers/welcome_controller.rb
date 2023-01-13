eda57a58f527b08607de15e188c8914a0d152358
Lucas-ASouza <lucasalves-x@hotmail.com>
2023-01-13 08:59:39 -0300

    Feat: home page has a button to change language
    Bonjour, we're going international, (I didn't add french tho)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:course] = "Ruby practicing, hello I'm a [COOKIE]"
+    session[:course] = "Ruby practicing, hello I'm a [SESSION]"
     @name = params[:name]
     @course = params[:course]
     #this get what is send from the view
